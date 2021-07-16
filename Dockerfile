92ad16aeaca6edb5eec1181261a14e306d9ebd4d
alonwolf <alonwolfy@gmail.com>
2021-07-16 17:50:20 +0300

    done

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
-FROM python:3
+FROM bitnami/python:3
 # LABEL maintainer "Alon Wolf <alonwolfy@gmail.com>"
 COPY ./src ./src
 COPY ./drivers ./drivers
 COPY requirements.txt /
+RUN pip install --upgrade pip
 RUN pip install -r /requirements.txt
+RUN echo "deb http://deb.debian.org/debian/ unstable main contrib non-free" >> /etc/apt/sources.list.d/debian.list
+RUN apt-get update
+RUN apt install -y firefox
+# RUN Y
 EXPOSE 8050
-CMD ["python", "./app.py"]
+CMD ["python", "src/app.py"]
--- src/app.py
+++ src/app.py
@@ -1,6 +1,18 @@
-import time
+# import dataiku
 
+import time
+import warnings
+
+warnings.filterwarnings('ignore')
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import numpy as np
+import pandas as pd
+import plotly.express as px
 import streamlit as st
+from dash.dependencies import Input, Output
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -10,13 +22,36 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 from clear_massages import clear
-from landing import is_landing_main, landing_mainloop
+from landing import is_landing_main, landing_get_image
 from taken import is_taken, log_out, refresh
 from utils import read_data, save_data
 
+main_style = {
+    'background': '#FFFFFF',
+    'font-family': "Arial"
+}
 
-@st.cache
+title_style = {
+    'background': '#5473FF',
+    'text': '#FFFFFF'
+}
+
+desc_style = {
+    'background': '#FFFFFF',
+    'text': '5473FF'
+}
+dropdown_style = {
+    'background': '#DDE5ED',
+    'text': '#221C35'
+}
+
+
 def init_driver():
+    # try:
+    #     driver
+    #     return driver
+    # except:
+    #     pass
     url = 'https://web.whatsapp.com/'
 
     # chrome_driver_path = 'chromedriver_win32/chromedriver.exe'
@@ -25,86 +60,153 @@ def init_driver():
 
     # driver = webdriver.Chrome(chrome_driver_path)
     # driver.get(url)
-
+    import platform
+    print(platform.platform())
     
     options = FirefoxOptions()
-    # options.add_argument("--headless")
-
-    driver = webdriver.Firefox(executable_path = 'geckodriver-v0.29.1-win64/geckodriver.exe', options=options)#, chrome_options=op)
+    options.add_argument("--headless")
+    import platform
+    from pathlib import Path
+    if 'indow' in platform.system():
+            driver = webdriver.Firefox(executable_path = str(Path('drivers') / 'geckodriver-v0.29.1-win64' / 'geckodriver.exe'), options=options)#, chrome_options=op)
+    else:
+        try:
+            driver = webdriver.Firefox(executable_path = str(Path('drivers') / 'geckodriver-v0.29.1-linux32' / 'geckodriver'), options=options)#, chrome_options=op)
+        except:
+            driver = webdriver.Firefox(executable_path = str(Path('drivers') / 'geckodriver-v0.29.1-linux64' / 'geckodriver'), options=options)#, chrome_options=op)
+    
     driver.get(url)
 
-    time.sleep(20)
-    url = driver.command_executor._url
-    session_id = driver.session_id 
+    time.sleep(5)
 
-    return url, session_id
+    # url = driver.command_executor._url
+    # session_id = driver.session_id 
 
-def get_driver(url, session_id):
-    driver = webdriver.Remote(command_executor=url,desired_capabilities={})
-    # driver.close()   # this prevents the dummy browser
-    driver.session_id = session_id
-    st.write(is_landing_main(driver))
     return driver
 
 
-def randomUserAgent():
-    from random_user_agent.user_agent import UserAgent
+# Initialise the app
+app = dash.Dash(__name__)
+driver = init_driver()
 
-    # software_types  = [SoftWareType.FIREFOX.value]
-    # software_names   = [SoftWareName.WEB_BROWSER.value]
-    user_agent_rotator = UserAgent()#(software_names=software_names,software_types=software_types)
-    ua = user_agent_rotator.get_random_user_agent()
-    return ua
 
-class SessionRemote(webdriver.Remote):
-    def start_session(self, desired_capabilities, browser_profile=None):
-        # Skip the NEW_SESSION command issued by the original driver
-        # and set only some required attributes
-        self.w3c = True
+app.layout = html.Div(
+    style={}, 
+    children=[
+        html.Div(id='dummy-output', style={'display':'none'}),
+        dcc.Interval(id='interval', interval=2000),
+        html.H1('Whatsapp Cleaner'),
+        html.Div(
+            id = 'main',
+            children = ''
+        ),
+    ]
+)
 
-def attach_to_session(executor_url, session_id):
-    
-    original_execute = webdriver.Remote.execute
-    # def new_command_execute(self, command, params=None):
-    #     if command == "newSession":
-    #         # Mock the response
-    #         return {'success': 0, 'value': None, 'sessionId': session_id}
-    #     else:
-    #         return original_execute(self, command, params)
-    new_command_execute = original_execute
-    # Patch the function before creating the driver object
-    WebDriver.execute = new_command_execute
-
-    ua = randomUserAgent()
-    opts = RemoteWebDriverOptions()
-    opts.add_argument(ua)
-
-    driver = SessionRemote(command_executor=executor_url, desired_capabilities={}, options = opts)
-    driver.session_id = session_id
-    # Replace the patched function with original function
-    WebDriver.execute = original_execute
-    return driver
-
-def app():
-    st.title('Whatsapp Cleaner')
-    
-    url, session_id = init_driver()
-    driver = attach_to_session(url, session_id)
-    
+@app.callback(Output('main', 'children'), Input('interval', 'n_intervals'),)
+def interval_update(n_intervals):
     if is_landing_main(driver):
-        st.write('load QR:')
-        landing_mainloop(driver)
+        # return html.Img(src=app.get_asset_url(landing_get_image(driver)), style={'height':'50%', 'width':'50%'})
+        return html.Img(src='data:image/png;base64{}'.format(landing_get_image(driver)))
     elif is_taken(driver):
-        st.write('משתמשים במקום אחר. מה לעשות?')
-        if st.button('Refresh'):
-            refresh(driver)
-        if st.button('Sign out'):
-            log_out(driver)
-        
+        return [
+            html.Button('Refresh', id='refresh-button'),
+            html.Button('Sign out', id='signout-button')
+        ]
     else:
-        if st.button('Clear !'):
-            clear(driver)
+        return html.Button('Clean', id='clean-button')
+
+@app.callback(Output('refresh-button', 'style'), Input('refresh-button', 'n_clicks'), prevent_initial_call=True)
+def refresh_button(n_clicks):
+    refresh(driver)
+    dash.exceptions.PreventUpdate("")
+    return {}
+
+@app.callback(Output('signout-button', 'style'), Input('signout-button', 'n_clicks'), prevent_initial_call=True)
+def signout_button(n_clicks):
+    log_out(driver)
+    dash.exceptions.PreventUpdate("")
+    return {}
+
+@app.callback(Output('clean-button', 'style'), Input('clean-button', 'n_clicks'), prevent_initial_call=True)
+def clean_button(n_clicks):
+    clear(driver)
+    dash.exceptions.PreventUpdate("")
+    return {}
 
 
-if __name__ == "__main__":
-    app()
+
+# app.layout = html.Div(
+#     ## Step 1: Define App Style
+#     style={
+#         'backgroundColor': main_style['background'],
+#         'font-family': main_style['font-family']
+#     },
+#     children=[
+#         html.H1(
+#             children='Total Sales by Country',
+#             style={
+#                 'backgroundColor': title_style['background'],
+#                 'color': title_style['text'],
+#                 'textAlign': 'center'}
+#         ),
+#         html.Div(
+#             children='This graph allows you to compare the total sales amount and campaign influence by country.',
+#             style={
+#                 'backgroundColor': desc_style['background'],
+#                 'color': desc_style['text'],
+#                 'textAlign': 'center'}
+#         ),
+#         html.Div(
+#             id = 'main'
+#         ),
+#         html.Label(
+#             children='Select countries:'
+#         ),
+#         dcc.Dropdown(
+#             id='country-dropdown',
+#             style={'color': dropdown_style['text']},
+#             options=[{'label': i, 'value': i} for i in sorted(df.country.unique())],
+#             # value=[i for i in sorted(df.country.unique())],
+#             multi=True
+#         )
+#     ]
+# )
+
+
+# @app.callback(Output(component_id='main', component_property='children'),
+#             [Input(component_id='country-dropdown', component_property='value')],
+#             prevent_initial_call=False
+#             )
+# def update_graph(input_value):
+#     if not input_value:
+#         input_value = []
+#     if len(input_value) > 0:
+#         updated_df = df[df['country'].isin(input_value)]
+#         fig = px.histogram(updated_df,
+#                         x="value",
+#                         color="country",
+#                         title="Sales by Country",
+#                         )
+#         return dcc.Graph(
+#             id='graph-output',
+#             figure = fig
+#         )
+#     elif len(input_value) == 0:
+#         return html.H2('הכנס!')
+#         # raise dash.exceptions.PreventUpdate
+
+# # Define the app
+# # app.layout = html.Div()
+# app.layout = html.Div(children=[
+#                       html.Div(className='row',  # Define the row element
+#                                children=[
+#                                   html.Div(className='four columns div-user-controls'),  # Define the left element
+#                                   html.Div(className='eight columns div-for-charts bg-grey')  # Define the right element
+#                                   ])
+#                                 ])
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server()
+
